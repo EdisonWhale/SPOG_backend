fc17c1ff25a0a845bf3e3db84d2e87f10ed1347e
Edison XU <103423072+EdisonWhale@users.noreply.github.com>
2026-07-05 23:59:06 -0400

    Create __init__.py

---
+++ src/app/enum/__init__.py
@@ -0,0 +1,15 @@
+from .AgentEnum import PlatformEnum, EnvironmentEnum, DataLevel
+from .UseCaseEnum import UseCaseStatus
+from .ProjectEnum import GitlabSyncRequestedStatusEnum
+
+
+__all__ = [
+    # Agent Enum
+    'PlatformEnum',
+    'DataLevel',
+    'EnvironmentEnum',
+    # Use Case Enum
+    'UseCaseStatus',
+    # Project Enum
+    'GitlabSyncRequestedStatusEnum'
+]
