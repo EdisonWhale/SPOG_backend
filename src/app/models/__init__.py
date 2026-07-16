18196bc80d53dd50fb4010220c218499655c08cb
Edison XU <103423072+EdisonWhale@users.noreply.github.com>
2026-07-15 20:54:49 -0400

    Update __init__.py

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -5,8 +5,9 @@ from app.models.schemas.AuthSchemas import CurrentUserResponse
 # SPOG Base - Model
 from app.models.base.SpogBaseModel import SpogBaseModel
 from app.models.base.CustomBaseModel import AuthorizableModel, CustomBaseModel
-
+from app.models.snow.AIGovernanceFormVariables import AIGovernanceFormVariables
 from app.models.cursor.CursorData import CursorData
+from app.models.job.GitLabSyncJob import GitLabSyncJob
 
 __all__ = [
     # Common - Schema
@@ -19,5 +20,9 @@ __all__ = [
     'CustomBaseModel',
     'SpogBaseModel',
     'AuthorizableModel',
-    'CursorData'
+    'CursorData',
+    #snow - Model
+    'AIGovernanceFormVariables'
+    # gitlab - Model
+    'GitLabSyncJob'
 ]
