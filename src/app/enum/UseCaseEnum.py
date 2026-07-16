f9fa7ce35a3a8afb7bf8f33336db01a33d8fe682
Edison XU <103423072+EdisonWhale@users.noreply.github.com>
2026-07-15 20:32:35 -0400

    Update UseCaseEnum.py

--- src/app/enum/UseCaseEnum.py
+++ src/app/enum/UseCaseEnum.py
@@ -1,6 +1,9 @@
 from enum import Enum
 
-class UseCaseStatus(str, Enum):
+class UseCaseStatusEnum(str, Enum):
     """Use case approval status."""
     PENDING = "Pending"
+    SUBMITTED = "Submitted"
+    ASSIGNED = "Assigned"
+    IN_PROGRESS = "In Progress"
     APPROVED = "Approved"
